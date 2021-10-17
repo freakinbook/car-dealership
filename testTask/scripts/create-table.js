fb01a03ca463d2cd2c9ffb8be4201d4b1fb8153b
Dmitrii Zviagin <60284863+freakinbook@users.noreply.github.com>
2021-10-17 17:18:05 +0300

    Add availability

--- testTask/scripts/create-table.js
+++ testTask/scripts/create-table.js
@@ -5,15 +5,25 @@ $("input").on("change", function(){
     $.get(fileURL, function(cars){
         $("input").after("<table/>");
         var table = $("table");
-        table.append("<tr><th>Brand</th><th>Model</th><th>Color</th></tr>");
+        table.append("<tr><th>Brand</th><th>Model</th><th>Color</th><th>Availability</th></tr>");
         for (var i = 0; i < cars.length; i++){
             var brand = cars[i]['brand'];
             var model = cars[i]['model'];
             var color = cars[i]['color'];
+            var availability = isAvailable();
             var row = $("<tr/>");
-            var newrow = "<td>" + brand + "</td><td>" + model + "</td><td>"+ color + "</td>";
+            var newrow = "<td>" + brand + "</td><td>" + model + "</td><td>"+ color + "</td><td>" + availability + "</td>";
             row.append(newrow);
             table.append(row);
         }
     });
 });
+
+function isAvailable(){
+    var random_boolean = Math.random() < 0.5;
+    var cheeky_line = "Call Dmitrii. Tell him he got the job."
+    if (random_boolean){
+        return "not available"
+    }
+    return "<a href = '#' onClick='alert(\"" + cheeky_line + "\")'>Call dealer</a>";
+}
